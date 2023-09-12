602880a32727f44e8587084109cfe22720c3a6c2
EMRE <EMRE@DESKTOP-D3VV2GT>
2023-09-12 22:23:34 +0300

    Add project files.

---
+++ RiseStudyCase.WebApi/Models/ContactModel.cs
@@ -0,0 +1,25 @@
+﻿namespace RiseStudyCase.WebApi.Models
+{
+    public class ContactModel
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public string Company { get; set; }
+
+        ContactType ContactType { get; set; }
+
+        public string ContactInfo { get; set; }
+
+    }
+
+    enum ContactType
+    {
+        PhoneNumber,
+        Email,
+        Location,
+    }
+}
---
+++ RiseStudyCase.WebApi/Models/ContactReportModel.cs
@@ -0,0 +1,11 @@
+﻿namespace RiseStudyCase.WebApi.Models
+{
+    public class ContactReportModel
+    {
+        public List<Dictionary<string, int>>? LocationInfo { get; set; }
+
+        public int? LocationContanctCount { get; set; }
+
+        public int? LocationContactPhoneNumberCount { get; set; }
+    }
+}
